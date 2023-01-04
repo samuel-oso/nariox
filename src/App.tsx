a9287fce3db2fa4a95da4c050d57c54de7aee82e
samuel-oso <ososamuel035@gmail.com>
2023-01-05 00:10:16 +0100

    fix: configured theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,56 @@
 import "./App.css";
-import { Button } from "@mantine/core";
+import {
+  Card,
+  Image,
+  Text,
+  Badge,
+  Button,
+  Group,
+  useMantineColorScheme,
+  // useMantineTheme,
+} from "@mantine/core";
+import ThemeToggle from "./components/ThemeToggle";
 
 function App() {
+  const { colorScheme } = useMantineColorScheme();
+  const dark = colorScheme === "dark";
+  // const theme = useMantineTheme();
+
   return (
-    <div className="App">
+    <div>
       <Button>Hello world!</Button>
+      <ThemeToggle />
+      <Card
+        sx={{ backgroundColor: dark ? "red" : "blue" }}
+        shadow="sm"
+        p="lg"
+        radius="md"
+        withBorder
+      >
+        <Card.Section>
+          <Image
+            src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+            height={160}
+            alt="Norway"
+          />
+        </Card.Section>
+
+        <Group position="apart" mt="md" mb="xs">
+          <Text weight={500}>Norway Fjord Adventures</Text>
+          <Badge color="pink" variant="light">
+            On Sale
+          </Badge>
+        </Group>
+
+        <Text size="sm" color="dimmed">
+          With Fjord Tours you can explore more of the magical fjord landscapes
+          with tours and activities on and around the fjords of Norway
+        </Text>
+
+        <Button variant="light" color="blue" fullWidth mt="md" radius="md">
+          Book classic tour now
+        </Button>
+      </Card>
     </div>
   );
 }
